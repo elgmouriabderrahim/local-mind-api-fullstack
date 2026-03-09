86cfb9df1fa5311a689e200a3f98944a3dd4efb5
elgmouriabderrahim <ea.officialbox@gmail.com>
2026-03-09 15:22:12 +0000

    add models

---
+++ backend/app/Models/Response.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    protected $fillable = ['content', 'user_id', 'question_id'];
+
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
